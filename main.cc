9d70f54ef0970cc888ef7a323fa92d132c60ee95
Andrew Whitaker <andrew.whitaker@mac.com>
2019-09-30 13:52:35 -0700

    Update workload

--- main.cc
+++ main.cc
@@ -25,6 +25,7 @@ using bsoncxx::builder::stream::finalize;
 using bsoncxx::builder::stream::open_array;
 using bsoncxx::builder::stream::open_document;
 using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_document;
 
 DEFINE_string(uri, "mongodb://localhost:27017/", "Database URI");
 DEFINE_string(workload, "insert", "Workload");
@@ -44,9 +45,10 @@ using WorkloadFunc = std::function<void(mongocxx::pool *, int32_t)>;
 
 static void InsertWorkload(mongocxx::pool *pool, int32_t index);
 static void FetchAllWorkload(mongocxx::pool *pool, int32_t index);
+static void UpdateWorkload(mongocxx::pool *pool, int32_t index);
 
 static const std::map<std::string, WorkloadFunc> workloads= {
-    {"insert", InsertWorkload}, {"find", FetchAllWorkload}
+    {"insert", InsertWorkload}, {"find", FetchAllWorkload}, {"update", UpdateWorkload}
 };
 
 static bool ValidateWorkload(const char* flagname, const std::string &value) {
@@ -71,9 +73,9 @@ static struct Statistics {
 
 static std::atomic<bool> shutdown_requested{false};
 
-static inline int64_t GetRandomDocumentIndex() {
+static inline int64_t GetRandomInt(int64_t max) {
     static thread_local std::mt19937 generator;
-    std::uniform_int_distribution<int64_t> distribution(0, FLAGS_num_documents - 1);
+    std::uniform_int_distribution<int64_t> distribution(0, max);
     return distribution(generator);
 }
 
@@ -97,14 +99,18 @@ static std::string CreateRandomString(size_t length) {
     return ret;
 }
 
+static inline std::string CreateKey(int32_t num) {
+    char key_num = '0' + num;
+    std::string key = "key";
+    key += key_num;
+    return key;
+}
+
 static bsoncxx::document::value CreateRandomDocument(int64_t id) {
     document doc{};
     doc << "_id" << bsoncxx::types::b_int64{id};
     for (int i=0; i < FLAGS_num_fields; i++) {
-        char key_num = '0' + i;
-        std::string key = "key";
-        key += key_num;
-        doc << key << CreateRandomString(FLAGS_field_length);
+        doc << CreateKey(i) << CreateRandomString(FLAGS_field_length);
     }
     return doc << finalize;
 }
@@ -132,10 +138,35 @@ static void FetchAllWorkload(mongocxx::pool *pool, int32_t index) {
     int64_t op = 0;
 
     while (!shutdown_requested) {
-        bsoncxx::types::b_int64 _id = {GetRandomDocumentIndex()};
+        bsoncxx::types::b_int64 _id = {GetRandomInt(FLAGS_num_documents - 1)};
         auto doc = bsoncxx::builder::basic::make_document(kvp("_id", _id));
 
         auto result = collection.find_one(doc.view());
+        INCREMENT_STAT(index, SUCCESSES);
+        if (++op >= num_ops) {
+            shutdown_requested = true;
+            break;
+        }
+    }
+}
+
+static void UpdateWorkload(mongocxx::pool *pool, int32_t index) {
+    auto client = pool->acquire();
+    mongocxx::collection collection = (*client)[FLAGS_db_name][FLAGS_coll_name];
+
+    const int64_t num_ops =  FLAGS_num_operations / FLAGS_num_threads;
+    int64_t op = 0;
+
+    while (!shutdown_requested) {
+        bsoncxx::types::b_int64 _id {GetRandomInt(FLAGS_num_documents - 1)};
+        std::string key = CreateKey(GetRandomInt(FLAGS_num_fields - 1));
+ 
+        collection.update_one(
+            make_document(kvp("_id", _id)),
+            make_document(kvp("$set",
+                              make_document(kvp(key,
+                                                CreateRandomString(FLAGS_field_length))))));
+ 
         INCREMENT_STAT(index, SUCCESSES);
         if (++op >= num_ops) {
             shutdown_requested = true;
